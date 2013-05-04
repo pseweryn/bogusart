2c163138e3df048cd7857aa592f7c13eb7ce1696
Pawel Seweryn <pseweryn@gmail.com>
2013-05-04 18:47:44 +0200

    version 0.1.8

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
 $(document).ready(function(){
+	$('.cycle').cycle({
+			random: 1,
+			fit: 0,
+			width: '300px'
+		});
 	$('.boxgrid').hover(function(){
 		$('.boxcaption', this).stop().animate({top:'200px'},{queue:false,duration:300});
 	}, function() {

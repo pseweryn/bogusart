36b1552924cf2440288d15ab68475d858ff054be
Pawel Seweryn <pseweryn@gmail.com>
2013-05-04 18:49:06 +0200

    version 0.1.9

--- js/main.js
+++ js/main.js
@@ -1,12 +1,19 @@
 $(document).ready(function(){
-	$('.cycle').cycle({
-			random: 1,
-			fit: 0,
-			width: '300px'
-		});
 	$('.boxgrid').hover(function(){
 		$('.boxcaption', this).stop().animate({top:'200px'},{queue:false,duration:300});
 	}, function() {
 		$('.boxcaption', this).stop().animate({top:'300px'},{queue:false,duration:300});
+	});
+	$('footer .row').hover(function(){
+		$('footer ul').slideDown();
+		$("html, body").animate({ scrollTop: $(document).height() }, 1000);
+	}, function() {
+		$('footer ul').slideUp();
+	});
+	$('header nav span').hover(function(){
+		$(this).next().slideDown();
+	}, function() {});
+	$('header nav').hover(function(){}, function() {
+		$('header ul').slideUp();
 	});
 });

3bbc37920d3b25731fe384330cf5aba20c76b2cc
pseweryn <pseweryn@gmail.com>
2013-06-09 15:07:58 +0200

    publish with names js

---
+++ publish/js/names.js
@@ -0,0 +1,17 @@
+	var categories = ['illustrations', 'drawings', 'graphics', 'photos', 'paintings', 'other'];
+	
+	var portofolio = ['illustr.jpg', 'draw.jpg', 'gfx.jpg', 'photo2.jpg', 'paint2.jpg', 'other.jpg'];
+	
+	var fader = ['1-clearcutfull.jpg', 'Cover-Full-JPG-cleaned.jpg', 'farwell-smaller.jpg', 'ff-Poster-Full-cleaned.jpg', 'k-N-AM-III-RGBb-cover.jpg', 'n-krzakowykonkret.jpg', 'regrowth.jpg', 'Reqiem.jpg', 'sharpmindXlarge.jpg', 'stokenewington9.jpg'];
+	
+	var illustrations = ['00clearcutfull.jpg', '00CoverFullCleaned.jpg', '00PosterFullcleaned.jpg', '01atomictree.jpg', '01farwellSmaller.jpg', '01healingFire.jpg', '01regrowthAndFrame.jpg', 'chmurkosluz.jpg', 'czachopismoBabcia.jpg', 'czachopismoPotworek.jpg', 'dwiewiorki.jpg', 'HHAAcopy.jpg', 'horror2.jpg', 'img063.jpg', 'img067.jpg', 'krzakowykonkret.jpg', 'Misio.jpg', 'N-AM_I_Ideas_and_Concepts.jpg', 'N-AM_II_Theory_and_Practice.jpg', 'N-AM_III_Methodology.jpg', 'Odkrywcy-AugustePiccard.jpg', 'Odkrywcy-RichardBurton-forweb.jpg', 'Odkrywcy-Statek.jpg', 'prapramiasto.jpg', 'sharpmind.jpg', 'stforki.jpg', 'wsz.jpg', 'XVXhands.jpg'];
+	
+	var drawings = ['IMG2_7009.jpg', 'ink_sketch.jpg', 'img040.jpg', 'sittingSquatter.jpg', 'wrrr.jpg', 'img083.jpg', 'Jess1.jpg', 'Jess2.jpg', 'Jess3.jpg', 'squattingSquatter.jpg', 'img042.jpg', 'judoSitting.jpg', 'sketchJudo.jpg', 'Untitled_Panorama1.jpg', 'szkicforweb.jpg', 'img038.jpg', 'IMG_6niou937.jpg', 'IMG_6939.jpg'];
+	
+	var graphics = ['Dyplom1.jpg', 'Dyplom2.jpg', 'Dyplom3.jpg', 'Dyplom4.jpg', 'Dyplom5.jpg', 'Dyplom6.jpg', 'LinorytPostPlener.jpg', 'Reqiem.jpg', '06.jpg', 'autoportret.jpg', 'newspaperscream.jpg', 'kloszard.jpg', 'budzaSie.jpg', 'testing8.jpg', 'testing9.jpg', 'testing3.jpg'];
+	
+	var photos = ['image023.jpg', 'img503.jpg', 'img504.jpg', 'random096.jpg', 'random099.jpg', 'image005.jpg', 'DSC05579.JPG', 'DSC02263.JPG', '105_0592.JPG', 'DSC03032.JPG', 'DSC02418.JPG', 'foto119a.jpg', 'DSC03009.JPG', 'foto010.jpg', 'foto022.jpg', 'img370a.jpg', 'img465.jpg', 'pix002.jpg', 'random207.jpg', 'foto071.jpg', 'foto070.jpg', 'foto048.jpg', 'IMG_0367.JPG', 'SN8507102.JPG', 'szaruga.jpg'];
+	
+	var paintings = ['20.jpg', '21.jpg', 'chata.jpg', 'domek1.jpg', 'karyn.jpg'];
+	
+	var other = ['poster_smaller.jpg', 'vasXe_jotpeg.jpg'];
